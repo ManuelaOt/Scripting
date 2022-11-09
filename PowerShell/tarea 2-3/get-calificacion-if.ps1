07795fca3487f0ecf8fea17f84db1fe6800be50f
miguel <99346525+LoSaMiASIR@users.noreply.github.com>
2022-11-09 17:34:39 +0100

    Add files via upload

---
+++ PowerShell/tarea 2-3/get-calificacion-if.ps1
@@ -0,0 +1,23 @@
+﻿<#
+.SYNOPSIS
+    Notas con if-else
+.DESCRIPTION
+    Pide al usuario una nota decimal y devuelve "suspenso", "aprobado", "bien", "notable", "sobresaliente" o "Error. Introduce una nota entre 0 y 10". Usa la estructura if-else
+.NOTES
+    File: get-calificacion-if.ps1
+    Author: Miguel Ángel López Sánchez
+    Date: 09/11/2022
+#>
+[double]$nota=Read-Host "Cual es la nota a comprobar "
+if ($nota -ge 0 -and $nota -lt 5){
+    Write-Host "Suspenso"}
+elseif ($nota -lt 6){
+    Write-Host "Aprobado"}
+elseif ($nota -le 7){
+    Write-Host "Bien"}
+elseif ($nota -lt 9){
+    Write-Host "Notable"}
+elseif ($nota -le 10){
+    Write-Host "Sobresaliente"}
+else{
+    Write-host "Inserta un numero comprendido entre 0 y 10"}
---
+++ PowerShell/tarea 2-3/get-laborable-finde-switch.ps1
@@ -0,0 +1,16 @@
+﻿<#
+.SYNOPSIS
+    Laborable o finde con switch
+.DESCRIPTION
+    Pide al usuario un día de la semana [1, 2... 7] y devuelve "laborable", "finde" o "Error. Introduce un día de la semana entre 1 y 7". Usa la estructura switch
+.NOTES
+    File: get-laborable-finde-switch.ps1
+    Author: Miguel Ángel López Sánchez
+    Date: 09/11/2022
+#>
+[int] $dia = Read-Host “¿Trabajaremos hoy?”
+Switch -wildcard ( $dia ) {
+    [1-5] { Write-Host “Laborable” }
+    [67] { Write-Host “Finde” }
+    Default { Write-Error “Error al introducir la nota”}
+}
